04944fc9e014a858deaa5216e550c5eb41b8c910
Lisa Vogt <lisavogt.sf@gmail.com>
2015-12-21 14:03:10 -0800

    dips into refs, using ref and getDOMNode (deprecated) to alert the new value in a text area

--- react-bulletin-board/js/Note.js
+++ react-bulletin-board/js/Note.js
@@ -9,6 +9,10 @@ var Note = React.createClass({
 		this.setState({editing: true});
 	},
 	save: function() {
+		// supposedly React getDOMNode has been deprecated in favor of ReactDOM.FindDOMNode
+		// but that isn't working either
+		var val = this.refs.newText.getDOMNode().value;
+		alert("TODO: save note value " + val);
 		this.setState({editing: false});
 	},
 	remove: function() {
@@ -32,7 +36,7 @@ var Note = React.createClass({
 	renderForm: function() {
 		return (
 			<div className="note">
-				<textarea defaultValue={this.props.children} className="form-control"></textarea>
+				<textarea ref="newText" defaultValue={this.props.children} className="form-control"></textarea>
 				<button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
 			</div>
 			);
