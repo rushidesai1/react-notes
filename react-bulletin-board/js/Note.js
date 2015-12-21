45acdbc6318f2954c286f3705166c891814753ac
Lisa Vogt <lisavogt.sf@gmail.com>
2015-12-21 11:47:50 -0800

    oops, forgot to add js directory before

---
+++ react-bulletin-board/js/Note.js
@@ -0,0 +1,28 @@
+var Note = React.createClass({
+	create: function() {
+		alert('creating new note');
+	},
+	edit: function() {
+		alert('editing note');
+	},
+	remove: function() {
+		alert('remove note');
+	},
+	render: function() {
+		return (
+			<div className="note">
+				<p>{this.props.children}</p>
+				<span>
+					<button onClick={this.create} 
+						className="btn btn-success glyphicon glyphicon-plus" />
+					<button onClick={this.edit} 
+						className="btn btn-primary glyphicon glyphicon-pencil" />
+					<button onClick={this.remove}
+						className="btn btn-danger glyphicon glyphicon-trash" />
+				</span>
+			</div>
+			);
+	}
+});
+
+React.render(<div><Note>Hello World</Note><Note>See ya later World</Note></div>, document.getElementById("react-container"));
